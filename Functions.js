bc745fab24fe0277d16658840566151ada79900f
Shivpalsingh15 <singhshivpal.coder@gmail.com>
2024-01-11 23:44:12 +0530

    functions practise

---
+++ Functions.js
@@ -0,0 +1,65 @@
+//syntax of Functions
+
+function hello(){
+    console.log('welcome to functions');
+}
+
+hello()
+hello()
+hello()
+
+
+
+ function sum(a, b){
+    console.log(a+b);
+ }
+sum(5, 8)
+sum(9, 7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sum(a, b){
+//     console.log(a+b)
+// }
+
+// sum('sum: ', 5, 2)
+
+// let add = ()=>{
+//     console.log('arrow functions');
+// }
+// add()
+
+// const noname = function(){
+//     console.log('function')
+// }
+// noname()
+
+// let iifn = (function(){
+//     console.log('shiv')
+// })()
